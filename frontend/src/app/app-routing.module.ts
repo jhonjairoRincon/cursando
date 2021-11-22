5949c1aa80d3d9b40919f78f18411c69ffbc176f
Jhon Jairo Rincón <jhonja.rs@gmail.com>
2021-11-22 08:00:10 -0500

    bosquejo frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LadingComponent } from './components/lading/lading.component';
+import { SinginComponent } from './components/singin/singin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component:LadingComponent
+  },
+  {
+    path: "singIn",
+    component: SinginComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CursoComponent } from './components/curso/curso.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LadingComponent } from './components/lading/lading.component';
+import { SinginComponent } from './components/singin/singin.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CursoComponent
+    CursoComponent,
+    HeaderComponent,
+    LadingComponent,
+    SinginComponent,
+ 
   ],
   imports: [
     BrowserModule,
